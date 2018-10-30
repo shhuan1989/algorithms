313280930f0ed1b8e01d0a6481ba925682065480
shuangquan, huang <shuangquanhuang@gmail.com>
2018-10-30 21:55:16 +0800

    add more

---
+++ codeforces/1065A.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import math
+import collections
+import bisect
+import heapq
+import time
+import random
+import itertools
+import sys
+
+"""
+created by shhuan at 2018/10/19 22:49
+
+"""

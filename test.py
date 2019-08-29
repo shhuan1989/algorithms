27062ef764325576829963068a1961255964b8c2
Shuangquan Huang <shuangquan.huang@hulu.com>
2019-08-29 11:08:29 +0800

    update

--- test.py
+++ test.py
@@ -6,22 +6,75 @@ import random
 import re
 import sys
 
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import numpy as np
+# cmap = cm.get_cmap('Blues')
+
+# w = np.random.rand(2, 1)
+w0 = np.array([0.6, 0.2])
+b0 = 0.1
+
+points = np.random.rand(20, 2)
+x = np.linspace(0, 1, 20).reshape(-1, 1)
+x = np.concatenate([x, np.ones_like(x)], axis=1)
+x[:, 0]
+y = x.dot(w0)+b0
+plt.plot(x[:,0], y)
 
 
+w = (y[1]-y[0]) / (x[1, 0] - x[0, 0])
+b = y[0] - w*x[0, 0]
+w, b
 
-with open('/Users/shuangquan.huang/Downloads/videos.txt') as f1:
-    a = [x.strip() for x in f1]
+cat = [1 if x > 0 else 0 for x in points[:, 1] - (w*points[:, 0] + b)]
+
+colors = ['r' if x > 0 else 'g' for x in cat]
+plt.scatter(x=points[:, 0], y=points[:, 1], c=cat)
+# print(cmap(0), cmap(1))
+print(x)
+print(y)
+plt.show()
+
+
+print(points.dot(np.array([w, b]).reshape(-1, 1)))
+
+
+w = np.random.rand(3, 1)
+y = np.array([1 if v == 1 else -1 for v in cat]).reshape(-1, 1)
+print(y)
+print(x.shape, y.shape, w.shape)
+alpha = 0.01
+N = len(y)
+costs = []
+x = np.c_[-points[:, 0], [-1]*len(points), points[:, 1]]
+for epoch in range(1000):
+    cost = 0.0
+    for i in range(len(y)):
+        rand_ind = np.random.randint(0, N)
+        xi = x[rand_ind].reshape(1, -1)
+        yi = y[rand_ind].reshape(1, 1)
+        yhat = np.sum(np.sign(xi.dot(w)))
+        yi = np.sum(yi)
+        cost += 1.0 / 2 / N * (1-yi*yhat) ** 2
+        a = (1-yi * yhat) * yi
+        w = w + 1.0 / N * alpha * a * (xi.reshape(-1, 1))
+        w[-1, 0] = 1
     
-    with open('/Users/shuangquan.huang/Downloads/k4url.txt') as f2:
-        b = [x.strip() for x in f2]
-        # print(a[0])
-        # print(b)
+    costs.append(cost)
         
-        ans = []
-        for x in b:
-            # print(x)
-            if any([y.find(x) >= 0 for y in a]):
-                continue
-            ans.append(x)
-        
-        print('\n'.join(ans))
+print(w)
+print(costs[-1])
+
+
+plt.plot([i for i in range(len(costs))], costs)
+plt.show()
+
+# print(np.sign(x[:, 1].reshape(-1, 1) - np.stack([x[:, 0], np.ones((len(x),))], axis=1).dot(w)))
+# print(np.sign(y))
+yhat = np.sign(x.dot(w))
+# print(np.sum(np.square(1-y*yhat)))
+print(np.c_[yhat, y])
+plt.scatter(x=-x[:, 0], y=x[:, 2], c=[1 if v > 0 else 0 for v in y.flatten()])
+# plt.plot([i for i in range(len(costs))], costs)
+plt.show()
